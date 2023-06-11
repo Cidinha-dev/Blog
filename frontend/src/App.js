01ecf62627aae15d797f6ef30f67c753d183704e
Aparecida <cidinha.dev@gmail.com>
2023-06-11 11:09:18 -0300

    Caminhos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-function App() {
-  return <div>Hello World!</div>;
-}
+//creador das rotas dos provedores
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+//caminho das paginas
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
+//provedor de rotas
+function App() {
+  return (
+  <div>
+   <RouterProvider router={router}/>
+  </div>);
+}
 export default App;
 
 
