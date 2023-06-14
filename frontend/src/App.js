803c373abd38218288ecdb8cdf2812241303fb4f
Aparecida <cidinha.dev@gmail.com>
2023-06-13 22:36:43 -0300

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,75 @@
 //creador das rotas dos provedores
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
+
+//paginas 
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Single } from "./pages/Single";
+import { Write } from "./pages/Write";
+//componentes
+import { Navebar } from "./components/Navebar";
+import { Footer } from "./components/Footer";
+//sass
+import "./style.scss"
+
+
+//função para renderizar a navebar e o footer e no meio o filho
+const Layout = ()=>{
+  return(
+    <>
+    <Navebar />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
 
 //caminho das paginas
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<Layout />,
+    children:[
+      {
+        path:"/home",
+        element:<Home />
+      },
+      {
+        path:"/post:id",
+        element:<Single />
+      },
+      {
+        path:"/write",
+        element:<Write />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element:<Login />
+  },
+  {
+    path: "/register", 
+    element: <Register />
   },
 ]);
 
 
-//provedor de rotas
+//provedor das rotas função app que renderiza a tela
 function App() {
   return (
-  <div>
-   <RouterProvider router={router}/>
-  </div>);
+  <div className="app">
+    <div className="container">
+      <RouterProvider router={router}/>
+   </div>
+  </div>
+  );
 }
 export default App;
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder='nome do usúario'/>
+        <input type="password" placeholder='senha'/>
+        <button>Login</button>
+        <span>Você não tem conta? <Link to="/register">Registre-se</Link></span>
+
+      </form>
+      
+    </div>
   )
 }
